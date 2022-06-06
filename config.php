dbfdd5170e63c027d7e6766e3d2939dfa079e3a6
lubHenry <104198325+lubHenry@users.noreply.github.com>
2022-06-06 17:14:45 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 
 // Define DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "p@ssw0rd");
-define("DB_NAME", "shareboard");
+define("DB_USER", "miny");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 
 // Define URL
-define("ROOT_PATH", "/php.dev/");
-define("ROOT_URL", "http://localhost/php.dev/");
+define("ROOT_PATH", "/home/");
+define("ROOT_URL", "http://localhost/home/");
